6654d871b0c65f5320e2c6a1fb8f1a2ece9bab11
NSDawn <tnahsin.airus@gmail.com>
2022-04-07 11:28:52 -0700

    screen with text!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // NS DAWN 
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     width: 640,
-    height: 480
-  }
+    height: 480,
+    scene: [ Menu, Play ]
+}
 
 let game = new Phaser.Game(config); 
