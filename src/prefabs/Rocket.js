8a25d13a1bc5a38a3a3a9cf58f0b7c5dd8e17958
NSDawn <tnahsin.airus@gmail.com>
2022-04-07 14:38:55 -0700

    gameplay finish

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,12 +1,13 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
+         this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+         super(scene, x, y, texture, frame);
   
-      // add object to existing scene
-      scene.add.existing(this);
-      this.isFiring = false;
-      this.moveSpeed = 2;
+        // add object to existing scene
+         scene.add.existing(this);
+         this.isFiring = false;
+         this.moveSpeed = 2;
     }
   
     update() {
@@ -21,7 +22,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // fire button
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
-            // this.sfxRocket.play();
+            this.sfxRocket.play();
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
